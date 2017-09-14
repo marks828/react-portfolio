04b77dfa81a2fa046ee82f9557b4dadd84ea4f54
Mark Sweet <marks828@gmail.com>
2017-09-14 11:02:40 -0400

    updated css

--- src/pages/projects/feedme.js
+++ src/pages/projects/feedme.js
@@ -9,7 +9,7 @@ class FeedMe extends Component{
         <div className="project-description">
           <h2 className="project-name">FeedMe</h2>
           <p>Find a restaurant near you!</p>
-          <p className="code"><Link to="#">Site</Link> | <Link to="#">Github</Link></p>
+          <p className="code"><Link to="https://marks828.github.io/project3_angular/#/feedme" target="_blank">Site</Link> | <Link to="https://github.com/marks828/project3" target="_blank">Github</Link></p>
         </div>
       </div>
     )
