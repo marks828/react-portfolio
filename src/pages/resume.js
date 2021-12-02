6227759cdd25d9f54916442fa23582e4101c7081
mark <marks828@gmail.com>
2021-12-02 08:31:48 -0500

    update resume

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -4,7 +4,7 @@ class Resume extends Component{
   render(){
     return(
       <div className="resume-container">
-        <img src={require('../img/mark_sweet_resume.svg')} alt="" className="resume"/>
+        <img src={require('../img/mark-sweet-resume.svg')} alt="" className="resume"/>
       </div>
     )
   }
